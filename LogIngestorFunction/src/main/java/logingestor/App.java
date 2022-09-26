b038a5aa190eae1374550bb614b931c487c08253
oixgres <a1252819@uabc.edu.mx>
2022-09-25 21:28:27 -0700

    Implemented json deserealization and class to manage postgres connection and queries

--- LogIngestorFunction/src/main/java/logingestor/App.java
+++ LogIngestorFunction/src/main/java/logingestor/App.java
@@ -4,35 +4,48 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+// generic utility class
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// AMAZON IMPORTS
+/* ***** Lambda ***** */
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/* ***** Time Stream ***** */
+// import com.amazonaws.services.timestreamwrite.model.Dimension;
+
 
 /**
  * Handler for requests to Lambda function.
  */
-public class App implements RequestHandler<Object, Object> {
+public class App implements RequestHandler<APIGatewayProxyRequestEvent , APIGatewayProxyResponseEvent> {
 
-    public Object handleRequest(final Object input, final Context context) {
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
         try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"%s\", \"location\": \"%s\" }", input, pageContents);
-            return new GatewayResponse(output, headers, 200);
-        } catch (IOException e) {
-            return new GatewayResponse("{}", headers, 500);
-        }
-    }
+            String inputBody = input.getBody();
+            ObjectMapper mapper = new ObjectMapper();
+            VisitLog log = mapper.readValue(inputBody, VisitLog.class);
+            DBManager db = new DBManager();
 
-    private String getPageContents(String address) throws IOException{
-        URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+            db.insertRecord(log);
+
+            String output = String.format("{\"response\": \"Log successfully stored\", \"visitlog\": \"%s\"}", log);
+            return new APIGatewayProxyResponseEvent().withHeaders(headers).withBody(output);
+        } catch (IOException e) {
+            return new APIGatewayProxyResponseEvent().withHeaders(headers).withBody(e.getMessage());
+        } catch (SQLException e) {
+            return new APIGatewayProxyResponseEvent().withHeaders(headers).withBody(e.getMessage());
         }
     }
 }
